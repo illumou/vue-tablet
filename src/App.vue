9ab784926f7001fd3cdab72795a6999f71c7646c
Robin <82582947+illumou@users.noreply.github.com>
2024-03-01 16:44:30 +0100

    refactor app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="ui_element_colors h-screen w-full bg-background_color_main">
-    <div class="h-full w-full  flex items-center justify-center">
-      <RouterView />
-    </div>
+  <div class="ui_element_colors h-screen w-full bg-background_color_main flex items-center justify-center">
+    <RouterView />
   </div>
 </template>
 
