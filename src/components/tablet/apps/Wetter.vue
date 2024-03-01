4e41fcdfb9ba8209001140fdf4da8aab4e285b0a
Robin <82582947+illumou@users.noreply.github.com>
2024-03-01 17:12:09 +0100

    Update Wetter.vue

--- src/components/tablet/apps/Wetter.vue
+++ src/components/tablet/apps/Wetter.vue
@@ -8,13 +8,13 @@
         <main class="w-full h-full flex flex-col items-center justify-center gap-2">
             <h1 class="w-full pl-14 text-xl text-text_color"> Wochenvorschau </h1>
             <div class="flex flex-row items-center justify-center gap-2">
-                <div v-for="entrie in data" class="w-40 h-96 bg-gray-200 rounded-xl flex flex-col p-2"
+                <div v-for="entrie in data" class="w-40 h-96 rounded-xl flex flex-col p-2"
                 :class="[getColorClass(entrie.mode)]">
                     <div class="w-full h-full bg-background_color_child rounded-lg flex flex-col items-center justify-between py-24">
                         <div>
-                            <i v-if="entrie.mode === 'Sun'" class="fa-regular fa-sun fa-2xl" style="color: #ffffff;"/>
-                            <i v-if="entrie.mode === 'Cloud'" class="fa-solid fa-cloud-sun fa-2xl" style="color: #ffffff;"/>
-                            <i v-if="entrie.mode === 'Rain'" class="fa-solid fa-cloud-rain fa-2xl" style="color: #ffffff;"/>
+                            <i v-if="entrie.mode === 'Sonne'" class="fa-regular fa-sun fa-2xl" style="color: #ffffff;"/>
+                            <i v-if="entrie.mode === 'Wolkig'" class="fa-solid fa-cloud-sun fa-2xl" style="color: #ffffff;"/>
+                            <i v-if="entrie.mode === 'Regen'" class="fa-solid fa-cloud-rain fa-2xl" style="color: #ffffff;"/>
                         </div>
                         <div class="text-text_color">
                             <p> {{ entrie.date }} </p>
@@ -35,27 +35,27 @@ export default {
     data() {
         return {
             data: [
-                {id: 1, mode: 'Sun', temp: 20, date: 'Montag'},
-                {id: 2, mode: 'Cloud', temp: 15, date: 'Dienstag'},
-                {id: 3, mode: 'Rain', temp: 16, date: 'Mittwoch'},
-                {id: 4, mode: 'Sun', temp: 24, date: 'Donnerstag'},
-                {id: 5, mode: 'Sun', temp: 20, date: 'Freitag'},
-                {id: 6, mode: 'Rain', temp: 20, date: 'Samstag'},
-                {id: 7, mode: 'Rain', temp: 21, date: 'Sonntag'},
+                {id: 1, mode: 'Sonne', temp: 20, date: 'Montag'},
+                {id: 2, mode: 'Sonne', temp: 15, date: 'Dienstag'},
+                {id: 3, mode: 'Regen', temp: 16, date: 'Mittwoch'},
+                {id: 4, mode: 'Sonne', temp: 24, date: 'Donnerstag'},
+                {id: 5, mode: 'Sonne', temp: 20, date: 'Freitag'},
+                {id: 6, mode: 'Regen', temp: 20, date: 'Samstag'},
+                {id: 7, mode: 'Regen', temp: 21, date: 'Sonntag'},
             ]
         }
     },
     methods: {
         getColorClass(mode) {
             switch (mode) {
-                case 'Sun': {
+                case 'Sonne': {
                     return 'bg-yellow-500'
                 }
-                case 'Cloud': {
-                    return 'blue-gray-600'
+                case 'Wolkig': {
+                    return 'bg-gray-500'
                 }
-                case 'Rain': {
-                    return 'bg-blue-600'
+                case 'Regen': {
+                    return 'bg-blue-500'
                 }
             }
         },
